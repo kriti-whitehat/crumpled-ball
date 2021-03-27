e4498793f6bae1b7e95b7fdaaab734124880d769
kriti-whitehat <69476922+kriti-whitehat@users.noreply.github.com>
2021-03-27 09:11:31 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,33 @@
+ class Paper {
+  constructor(x,y,r)
+  
+{
+  var options={
+    isStatic:false,
+      restitution:0.3,
+      friction:0,
+      density:1.3
+  }
+  this.x=x
+  this.y=y
+  this.r=r
+  this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+  World.add(world, this.body)
+}
+display(){
+  this.image=loadImage("paper.png")
+  var paperpos=this.body.position
+  push()
+  translate(paperpos.x, paperpos.y)
+  rectMode(CENTER)
+  strokeWeight(4)
+  stroke("black")
+  fill("gray")
+  ellipse(0,0,this.r, this.r)
+  pop()
+}
+}
+
+  
+    
+    
